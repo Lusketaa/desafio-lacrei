7caf838f06a129f5c448d20c5e380a6f3d30903b
LuccaDev <102116096+Lusketaa@users.noreply.github.com>
2024-04-12 16:02:51 -0300

    feat: Completing the coding, styling, and responsiveness of the Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,45 @@
-import { Wrapper, Logo, Nav, ButtonBase, NavButton } from './styles';
+import { useState } from 'react';
+import { Wrapper, Logo, Nav, HamburguerMenu, NavMenuMobile, NavMenuContent, ButtonBase, NavButton } from './styles';
+import { IoClose } from "react-icons/io5";
+import { MdMenu } from "react-icons/md";
 
-export default function Header () {
+export default function Header() {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpenMenu(!isOpenMenu);
+  };
+
   return (
     <Wrapper>
       <Logo />
+      <HamburguerMenu>
+        {isOpenMenu ? (
+          <IoClose 
+            size={32}
+            onClick={toggleMenu} 
+            style={{zIndex: 999, color: '#018762'}}
+          />
+        ) : (
+          <MdMenu
+            size={32}
+            onClick={toggleMenu}
+            style={{zIndex: 999, color: '#018762' }} 
+          />
+        )}
+      </HamburguerMenu>
+      {isOpenMenu && (
+        <NavMenuMobile>
+          <NavMenuContent>
+            <NavButton href='/'>Quem somos</NavButton>
+            <NavButton href='/'>Ajuda</NavButton>
+            <ButtonBase>Entrar</ButtonBase>
+          </NavMenuContent>
+        </NavMenuMobile>
+      )}
       <Nav>
-        <NavButton>Quem somos</NavButton>
-        <NavButton>Ajuda</NavButton>
+        <NavButton href='/'>Quem somos</NavButton>
+        <NavButton href='/'>Ajuda</NavButton>
         <ButtonBase>Entrar</ButtonBase>
       </Nav>
     </Wrapper>
--- src/components/Header/styles.tsx
+++ src/components/Header/styles.tsx
@@ -1,11 +1,14 @@
 import styled from 'styled-components';
 import logo from '../../assets/logo.svg';
+import { colors, breakpoints } from '../../styles/Variables';
 
 export const Wrapper = styled.header`
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding: 24px;
+
+    
 `;
 
 export const Logo = styled.a`
@@ -14,7 +17,7 @@ export const Logo = styled.a`
   background-image: url(${logo});
   background-size: contain;
   background-repeat: no-repeat;
-  background-position: center; /* Centraliza a imagem */
+  background-position: center; 
   cursor: pointer;
   transition: linear .3s;
 
@@ -27,11 +30,51 @@ export const Nav = styled.nav`
   display: flex;
   align-items: center;
   gap: 16px;
+
+  @media screen and (max-width: ${breakpoints.medium}) {
+    display: none;
+  };
 `
 
+export const HamburguerMenu = styled.div`
+  display: none;
+
+  @media screen and (max-width: ${breakpoints.medium}) {
+    display: block;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 999;
+  };
+`
+
+export const NavMenuMobile = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 10;
+`;
+
+export const NavMenuContent = styled.div`
+  background-color: ${colors.white}; 
+  width: 100%;
+  height: 100vh;
+
+  padding: 100px;
+  display: flex;
+  flex-direction: column; 
+  align-items: center;
+  gap: 20px;
+`;
+
 export const ButtonBase = styled.button`
-  background-color: #018762;
-  color: #ffffff;
+  background-color: ${colors.primary};
+  color: ${colors.white};
   border: none;
   padding: 10px 32px;
   border-radius: 8px;
@@ -45,24 +88,36 @@ export const ButtonBase = styled.button`
   box-shadow: 0px 5px 10px 0px #0000004D;
 
   &:hover {
-    background-color: #0b9a71;
+    background-color: ${colors.primaryLight};
   }
+
+  @media screen and (max-width: ${breakpoints.medium}) {
+    width: 100%;
+  };
 `;
 
 export const NavButton = styled.a`
-  color: #018762;
-  background-color: #ffffff;
+  color: ${colors.primary};
+  background-color: ${colors.white};
   padding: 10px 32px;
   border-radius: 8px;
 
   font-size: 18px;
   line-height: 27px;
   font-weight: 700;
+  text-decoration: none;
 
   cursor: pointer;
   transition: linear .3s;
 
   &:hover {
-    color: #0b9a71;
+    color: ${colors.primaryLight};
   }
+
+  @media screen and (max-width: ${breakpoints.medium}) {
+    width: 100%;
+    text-align: center;
+  };
+  
 `;
+
