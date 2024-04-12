0ac1fd8300cb9410b0f85b2fd1a6a7c5a86b81f0
LuccaDev <102116096+Lusketaa@users.noreply.github.com>
2024-04-12 16:03:58 -0300

    feat: Completing the coding, styling, and responsiveness of the Main component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,24 @@
+import { ContainerContent, ContainerImg, Wrapper, Title, Subtitle, ContainerButton, Button, CustomButton, Line, Img } from "./styles";
+import img from '../../assets/img.svg';
+
 export default function Main() {
 
     return (
       <>
-        <h1>Main</h1>  
+        <Wrapper>
+          <ContainerContent>
+            <Title>Junte-se à nossa comunidade</Title>
+            <Line />
+            <Subtitle>Encontre atendimento clínico de qualidade ou entre para o time de profissionais da Lacrei Saúde. </Subtitle>
+            <ContainerButton>
+              <Button>Buscar atendimento</Button>
+              <CustomButton>Oferecer atendimento</CustomButton>
+            </ContainerButton>
+          </ContainerContent>
+          <ContainerImg>
+            <Img src={img} alt="Imagem de atendimento ao paciente"/>
+          </ContainerImg>
+        </Wrapper>
       </>
     )
 }
