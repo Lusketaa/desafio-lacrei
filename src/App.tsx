80d126af700ea6b969f918b0302f8de3e41cb9df
LuccaDev <102116096+Lusketaa@users.noreply.github.com>
2024-04-12 16:05:35 -0300

    feat: Adding the assets of the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import GlobalStyle from "./styles/global";
+import GlobalStyle from "./styles/GlobalStyle";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
---
+++ src/styles/Variables.tsx
@@ -0,0 +1,14 @@
+export const colors = {
+    primary: '#018762',
+    primaryLight: '#0b9a71',
+    primaryLighten: '#B2DFD0',
+    white: '#f1f3f5',
+    black: '#2D2D2D',
+};
+
+export const breakpoints = {
+    large: '1200px',
+    medium: '960px',
+    small: '620px',
+};
+
