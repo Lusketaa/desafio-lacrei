f3a05b4ac80365e1a23038412848417d5198b9c3
LuccaDev <102116096+Lusketaa@users.noreply.github.com>
2024-04-12 23:52:09 -0300

    fix: fixing the deploy error

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import React from 'react';
+
 import GlobalStyle from "./styles/GlobalStyle";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 
-export default function App() {
+const App: React.FC = () => {
 
   return (
     <>
@@ -16,4 +18,4 @@ export default function App() {
   )
 }
 
-
+export default App;
